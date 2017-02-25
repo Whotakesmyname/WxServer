a6d6b88563f397d95f214a04e5eb8379ea8b9f2e
Darkray <darkray@126.com>
2017-02-26 02:39:36 +0800

    optimized

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def add_friends(msg):
     一个并不成熟的工具，希望能帮助到(北外的)你\n
     首次使用查询成绩、快速选课等功能前，请回复login进行登录\n
     回复 h 获得进一步帮助\n
-    源码请访问项目主页：还没建好\n'''
+    源码请访问项目主页：https://github.com/Whotakesmyname/WxServer\n'''
     itchat.add_friend(**msg['Text'])
     # occupied for check wechatid
     itchat.send_msg(welcomemsg, msg['RecommendInfo']['UserName'])
@@ -31,7 +31,7 @@ status_lock = threading.RLock()
 status_queue = {'login': login_queue, 'get_score': get_score_queue, 'select_course': select_course_queue}
 welcome_msgs = {'login': '请回复你的学号',
                 'get_score': '请回复要查询的学期，\n如2016年秋季学期请回复\n2016a\ns(spring)=春；a(autumn)=秋\n回复其他任意信息将默认查询上一学期的成绩',
-                'select_course': '***须知\n  工具未经大量测试谨慎使用\n    请输入课程的课程号(及课序号，一般为1，不填默认为1)，你可以从通知文件中获取、从其他已登入系统的同学处窥得……\n输入格式为\n 课程号[;课序号]\n    方括号内为选填内容，若选填课序号请将二者用分号隔开\n    *根据教务处通知，本学期校通选课为区分2014级/2015 16级，出现多门通选不同模块相同课程相同课程号相同课序号，由此导致的后果未知；因此选课成功后请务必人工登录教务系统确认，后果自负'}
+                'select_course': '***须知\n  工具未经大量测试谨慎使用\n    请输入课程的课程号(及课序号，一般为1，不填默认为1)，你可以从通知文件中获取、从其他已登入系统的同学处窥得……\n输入格式为\n 课程号[;课序号]\n    方括号内为选填内容，若选填课序号请将二者用分号隔开\n    *根据教务处通知，本学期校通选课为区分2014级/2015 16级，出现多门通选不同年级对应不同模块但拥有相同课程号相同课序号，由此导致的后果未知；因此选课成功后请务必人工登录教务系统确认，后果自负'}
 
 query_pool = LastUpdatedOrderedDict()  # {'stuid': Query}
 query_pool_lock = threading.RLock()
@@ -59,11 +59,13 @@ def text_reply(msg):
           在首次使用教务相关功能前请回复 login 进行登录
           请注意，当前选课前必须知道目标课程的课程编号！
               成绩查询 请回复 查分
-              快速选课 请回复 选课
+              快速选课 请回复 选课*
           除此以外的消息你将会和一个智力低下的机器人聊天，据说她能查天气询时事之类……诸位自行探索
-          开发细节和碎碎念请回复 -help 获取
+          开发细节和碎碎念请回复 -help 获取（我还没写）
                            2017/1/30'''
         return helpmsg
+    elif msg['Text'] == '-help':
+        return '说了我还没写'
     else:
         return autoreply(msg['Text'], msg['FromUserName'])  # needed to be optimized
 
